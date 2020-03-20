7b1a68af7438338d52f4b23143b8faa1a0078bd5
Misha <mishabeheka@gmail.com>
2020-03-20 16:23:53 +0200

    Created dto-models

---
+++ src/main/java/web/system/task/dto/SolutionModelResponseDto.java
@@ -0,0 +1,59 @@
+package web.system.task.dto;
+
+public class SolutionModelResponseDto {
+
+    private Double a;
+    private Double b;
+    private Double c;
+    private Double d;
+    private Double x1;
+    private Double x2;
+
+    public Double getA() {
+        return a;
+    }
+
+    public void setA(Double a) {
+        this.a = a;
+    }
+
+    public Double getB() {
+        return b;
+    }
+
+    public void setB(Double b) {
+        this.b = b;
+    }
+
+    public Double getC() {
+        return c;
+    }
+
+    public void setC(Double c) {
+        this.c = c;
+    }
+
+    public Double getD() {
+        return d;
+    }
+
+    public void setD(Double d) {
+        this.d = d;
+    }
+
+    public Double getX1() {
+        return x1;
+    }
+
+    public void setX1(Double x1) {
+        this.x1 = x1;
+    }
+
+    public Double getX2() {
+        return x2;
+    }
+
+    public void setX2(Double x2) {
+        this.x2 = x2;
+    }
+}
